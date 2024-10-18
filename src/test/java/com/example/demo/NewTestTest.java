eacac052b3854142bc4cf405a40fa28080f43baf
Nameless1004 <jjho1029@gmail.com>
2024-10-18 18:44:19 +0900

    test

---
+++ src/test/java/com/example/demo/NewTestTest.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NewTestTest {
+    private NewTest newTest = new NewTest();
+
+    @Test
+    public void test() throws Exception {
+        // given
+        newTest.some();
+        // when
+
+        // then
+    }
+}
