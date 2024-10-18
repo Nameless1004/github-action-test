ddefb2bbf169675282cd45c84c34e3c60e5fd71c
Nameless1004 <jjho1029@gmail.com>
2024-10-19 02:04:37 +0900

    test

---
+++ src/test/java/com/example/demo/NewClassTest.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NewClassTest {
+    NewClass newClass = new NewClass();
+
+    @Test
+    public void tt() throws Exception {
+        // given
+        newClass.test();
+        // when
+
+        // then
+    }
+}
