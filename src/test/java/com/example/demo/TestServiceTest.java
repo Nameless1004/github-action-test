98318d95e430cc0d88977bee0229ae06c6129c5b
Nameless1004 <jjho1029@gmail.com>
2024-10-18 16:50:59 +0900

    test

--- src/test/java/com/example/demo/TestServiceTest.java
+++ src/test/java/com/example/demo/TestServiceTest.java
@@ -1,10 +1,38 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class TestServiceTest {
 
+    @Autowired
+    private TestService testService;
 
+    @Test
+    public void sum() throws Exception {
+        // given
+
+        // when
+        int sum = testService.sum(1, 5);
+
+        // then
+        assertThat(sum).isEqualTo(6);
+    }
+
+
+    @Test
+    public void mul() throws Exception {
+        // given
+
+        // when
+        int mul = testService.mul(2, 5);
+
+        // then
+        assertThat(mul).isEqualTo(10);
+    }
 }
